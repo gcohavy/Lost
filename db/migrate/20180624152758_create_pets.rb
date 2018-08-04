a0e389bdd053df6f9e5a843ac9c3edac6eb7b0c9
gcohavy <39204949+gcohavy@users.noreply.github.com>
2018-08-04 12:03:24 -0700

    Update 20180624152758_create_pets.rb

--- db/migrate/20180624152758_create_pets.rb
+++ db/migrate/20180624152758_create_pets.rb
@@ -1,22 +1,15 @@
 class CreatePets < ActiveRecord::Migration[5.1]
-
+  
   def change
-
+    
     create_table :pets do |t|
-
       t.string :location
-
       t.string :phone
       t.string :email
-
       t.text :description
 
-
-
       t.timestamps
-
     end
-
+    
   end
-
 end
